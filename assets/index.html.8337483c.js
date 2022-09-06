0828629f5ae48f584fce284cc13fc3ca298a02cc
keyL-liucong <53336284+keyL-liucong@users.noreply.github.com>
2022-09-06 04:56:24 +0000

    Deploying to gh-pages from  @ e322fcd292c3e0efbcc865e63fd162717c9226b7 🚀

---
+++ assets/index.html.8337483c.js
@@ -0,0 +1,30 @@
+import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as n,c as i,e as s}from"./app.407e407c.js";const l={},d=s(`<h1 id="\u5E38\u7528\u547D\u4EE4" tabindex="-1"><a class="header-anchor" href="#\u5E38\u7528\u547D\u4EE4" aria-hidden="true">#</a> \u5E38\u7528\u547D\u4EE4</h1><h2 id="centos-\u9632\u706B\u5899\u76F8\u5173" tabindex="-1"><a class="header-anchor" href="#centos-\u9632\u706B\u5899\u76F8\u5173" aria-hidden="true">#</a> centos \u9632\u706B\u5899\u76F8\u5173</h2><div class="language-Bash ext-Bash line-numbers-mode"><pre class="language-Bash"><code># 1\u3001\u67E5\u770B\u9632\u706B\u5899\u72B6\u6001
+firewall-cmd --state
+
+# 2\u3001\u67E5\u770B\u9632\u706B\u5899\u6240\u6709\u5F00\u653E\u7684\u7AEF\u53E3
+firewall-cmd --zone=public --list-ports
+
+# 3.\u3001\u5F00\u5173\u9632\u706B\u5899
+systemctl start firewalld.service
+systemctl stop firewalld.service
+systemctl restart firewalld.service
+
+# 4\u3001\u5F00\u653E\u7AEF\u53E3
+firewall-cmd --zone=public --add-port=80/tcp --permanent # \u5F00\u653E80\u7AEF\u53E3
+firewall-cmd --zone=public --remove-port=80/tcp --permanent #\u5173\u95ED80\u7AEF\u53E3
+firewall-cmd --reload # \u914D\u7F6E\u7ACB\u5373\u751F\u6548
+
+# 5\u3001\u67E5\u770B\u76D1\u542C\u7684\u7AEF\u53E3
+netstat -tunlp
+
+# \u25CFcentos7\u9ED8\u8BA4\u6CA1\u6709 netstat \u547D\u4EE4\uFF0C\u9700\u8981\u5B89\u88C5 net-tools \u5DE5\u5177\uFF0Cyum install -y net-tools
+
+# 6\u3001\u68C0\u67E5\u7AEF\u53E3\u88AB\u54EA\u4E2A\u8FDB\u7A0B\u5360\u7528
+netstat -lnpt |grep 80
+
+# 7\u3001\u67E5\u770B\u8FDB\u7A0B\u7684\u8BE6\u7EC6\u4FE1\u606F
+ps 6832
+
+# 8\u3001\u4E2D\u6B62\u8FDB\u7A0B
+kill -9 6832
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),a=[d];function r(c,v){return n(),i("div",null,a)}var u=e(l,[["render",r],["__file","index.html.vue"]]);export{u as default};
---
+++ assets/wechat-account.html.89b29779.js
@@ -0,0 +1,51 @@
+import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";import{r as s,o as u,c as d,a as e,b as i,d as n,e as a}from"./app.407e407c.js";var l="/assets/wechat-account-2.93e09897.png",c="/assets/wechat-account-3.893f2169.png";const r={},v=e("h1",{id:"\u5FAE\u4FE1\u516C\u4F17\u53F7\u5F00\u53D1\u8FC7\u7A0B\u8BB0\u5F55",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#\u5FAE\u4FE1\u516C\u4F17\u53F7\u5F00\u53D1\u8FC7\u7A0B\u8BB0\u5F55","aria-hidden":"true"},"#"),n(" \u5FAE\u4FE1\u516C\u4F17\u53F7\u5F00\u53D1\u8FC7\u7A0B\u8BB0\u5F55")],-1),q=e("h2",{id:"\u7533\u8BF7\u516C\u4F17\u53F7\u6D4B\u8BD5\u53F7",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#\u7533\u8BF7\u516C\u4F17\u53F7\u6D4B\u8BD5\u53F7","aria-hidden":"true"},"#"),n(" \u7533\u8BF7\u516C\u4F17\u53F7\u6D4B\u8BD5\u53F7")],-1),h={href:"https://mp.weixin.qq.com/debug/cgi-bin/sandbox?t=sandbox/login",target:"_blank",rel:"noopener noreferrer"},_=n("\u6D4B\u8BD5\u516C\u4F17\u53F7\u7533\u8BF7\u5730\u5740"),m=e("p",null,"\u586B\u5B8C\u4EE5\u4E0A\u4FE1\u606F\u5373\u53EF\u5B8C\u6210\u6D4B\u8BD5\u516C\u4F17\u53F7\u7684\u7533\u8BF7\uFF0C\u6D4B\u8BD5\u516C\u4F17\u53F7\u636E\u6709\u6B63\u5E38\u670D\u52A1\u53F7\u7684\u529F\u80FD\uFF0C\u5177\u4F53\u529F\u80FD\u53C2\u8003\u4EE5\u4E0A\u94FE\u63A5\u7684\u4F53\u9A8C\u63A5\u53E3\u6743\u9650\u8868\u3002",-1),X=e("p",null,"\u6CE8\u610F\u4E8B\u9879\uFF1A",-1),p=e("ul",null,[e("li",null,"\u5FAE\u4FE1\u9700\u8981\u6821\u9A8C\u540E\u7AEF\u670D\u52A1\u7684\u53EF\u7528\uFF0C\u63A5\u53E3\u914D\u7F6E\u4FE1\u606F\u586B\u5199\u540E\u7AEF\u7684\u63A5\u53E3"),e("li",null,"token\u53EF\u4EE5\u968F\u4FBF\u5199\uFF0C\u540E\u7AEF\u4EE3\u7801\u5199\u4E0A\u5BF9\u5E94\u7684token\u5373\u53EF"),e("li",null,"js\u57DF\u540D\u5B89\u5168\u57DF\u540D\u586B\u5199\u524D\u7AEF\u9879\u76EE\u7684\u6D4B\u8BD5\u5730\u5740\u5373\u53EF")],-1),b=e("h2",{id:"\u83B7\u53D6\u516C\u4F17\u53F7access-token",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#\u83B7\u53D6\u516C\u4F17\u53F7access-token","aria-hidden":"true"},"#"),n(" \u83B7\u53D6\u516C\u4F17\u53F7access_token")],-1),f={href:"https://mp.weixin.qq.com/debug?token=602053699&lang=zh_CN",target:"_blank",rel:"noopener noreferrer"},x=n("\u5FAE\u4FE1\u516C\u4F17\u5E73\u53F0\u63A5\u53E3\u8C03\u8BD5\u5DE5\u5177"),g=a('<p>\u6839\u636E\u4EE5\u4E0A\u5E73\u53F0\u63A5\u53E3\u8C03\u8BD5\u5DE5\u5177\u586B\u5165\u76F8\u5173\u7684\u4FE1\u606F\uFF0C\u8C03\u7528access_token\u63A5\u53E3\u83B7\u53D6 access_token</p><p><img src="'+l+`" alt="\u83B7\u53D6access_token" loading="lazy"></p><h2 id="\u521B\u5EFA\u6D4B\u8BD5\u516C\u4F17\u53F7\u7684\u83DC\u5355" tabindex="-1"><a class="header-anchor" href="#\u521B\u5EFA\u6D4B\u8BD5\u516C\u4F17\u53F7\u7684\u83DC\u5355" aria-hidden="true">#</a> \u521B\u5EFA\u6D4B\u8BD5\u516C\u4F17\u53F7\u7684\u83DC\u5355</h2><p>\u83B7\u53D6 access_token \u540E\u5373\u53EF\u901A\u8FC7\u4EE5\u4E0A\u94FE\u63A5\u8C03\u7528\u521B\u5EFA\u83DC\u5355\u63A5\u53E3\uFF0C\u4EE5\u4E0B\u586B\u5165\u5177\u4F53\u9700\u8981\u6D4B\u8BD5\u7684\u94FE\u63A5\u5373\u53EF</p><div class="language-Bash ext-Bash line-numbers-mode"><pre class="language-Bash"><code>{
+    &quot;button&quot;: [
+        {
+            &quot;name&quot;: &quot;\u751F\u4EA7\u73AF\u5883&quot;,
+            &quot;sub_button&quot;: [
+                {
+                    &quot;type&quot;: &quot;view&quot;,
+                    &quot;name&quot;: &quot;\u58F9\u9A6CXXX&quot;,
+                    &quot;url&quot;: &quot;https://XXXXXXX/home&quot;
+                },
+                {
+                    &quot;type&quot;: &quot;view&quot;,
+                    &quot;name&quot;: &quot;\u6C5F\u94C3XXX&quot;,
+                    &quot;url&quot;: &quot;https://XXXXX/home&quot;
+                },
+                {
+                    &quot;type&quot;: &quot;view&quot;,
+                    &quot;name&quot;: &quot;\u9A8F\u6D3EXXX&quot;,
+                    &quot;url&quot;: &quot;https://XXXXXX/home&quot;
+                }
+            ]
+        },
+        {
+            &quot;name&quot;: &quot;\u6D4B\u8BD5\u73AF\u5883&quot;,
+            &quot;sub_button&quot;: [
+                {
+                    &quot;type&quot;: &quot;view&quot;,
+                    &quot;name&quot;: &quot;\u8F66\u97F3-XXX&quot;,
+                    &quot;url&quot;: &quot;https://XXXXX/home&quot;
+                },
+                {
+                    &quot;type&quot;: &quot;view&quot;,
+                    &quot;name&quot;: &quot;\u58F9\u9A6CXXX&quot;,
+                    &quot;url&quot;: &quot;https://XXXXXXXXX/home&quot;
+                },
+                {
+                    &quot;type&quot;: &quot;view&quot;,
+                    &quot;name&quot;: &quot;\u6C5F\u94C3XXX&quot;,
+                    &quot;url&quot;: &quot;https://XXXXXXXXX/home&quot;
+                },
+                {
+                    &quot;type&quot;: &quot;view&quot;,
+                    &quot;name&quot;: &quot;\u9A8F\u6D3EXXX&quot;,
+                    &quot;url&quot;: &quot;http://XXXXXXXXXXXXXXX/home&quot;
+                }
+            ]
+        }
+    ]
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u7528\u6237\u540C\u610F\u6388\u6743-\u83B7\u53D6code" tabindex="-1"><a class="header-anchor" href="#\u7528\u6237\u540C\u610F\u6388\u6743-\u83B7\u53D6code" aria-hidden="true">#</a> \u7528\u6237\u540C\u610F\u6388\u6743\uFF0C\u83B7\u53D6code</h2>`,6),k={href:"https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect",target:"_blank",rel:"noopener noreferrer"},w=n("https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect"),E=e("p",null,"\u82E5\u63D0\u793A\u201C\u8BE5\u94FE\u63A5\u65E0\u6CD5\u8BBF\u95EE\u201D\uFF0C\u8BF7\u68C0\u67E5\u53C2\u6570\u662F\u5426\u586B\u5199\u9519\u8BEF\uFF0C\u662F\u5426\u62E5\u6709 scope \u53C2\u6570\u5BF9\u5E94\u7684\u6388\u6743\u4F5C\u7528\u57DF\u6743\u9650\u3002",-1),y=e("p",null,"\u5982\u679C\u7528\u6237\u540C\u610F\u6388\u6743\uFF0C\u9875\u9762\u5C06\u8DF3\u8F6C\u81F3 redirect_uri/?code=CODE&state=STATE\uFF0C\u5728\u6211\u4EEC\u5177\u4F53\u4E1A\u52A1\u83B7\u53D6 access_token \u65F6\u5019\uFF0C\u8BF7\u6C42\u6210\u529F\u65F6\uFF0C\u5FAE\u4FE1\u5E73\u53F0\u4F1A\u56DE\u8C03reirect_uri\u914D\u7F6E\u7684\u5730\u5740\uFF0C\u5E76\u5C06code\u8FFD\u52A0\u5230\u8DEF\u5F84\u4E0A\u3002\u683C\u5F0F\u4E3A\uFF1Aredirect_uri?code=xxx\u3002",-1),T=e("p",null,"\u5177\u4F53\u53C2\u8003\uFF1A",-1),A={href:"https://developers.weixin.qq.com/doc/offiaccount/OA_Web_Apps/Wechat_webpage_authorization.html#0",target:"_blank",rel:"noopener noreferrer"},I=n("\u7528\u6237\u540C\u610F\u6388\u6743\uFF0C\u83B7\u53D6code"),C=e("p",null,"code =\u300B \u540E\u7AEF\u62FF\u5230code =\u300B \u5FAE\u4FE1\u670D\u52A1\u5668\u83B7\u53D6 access_token",-1),z=e("pre",null,[e("code",null,`\u300A==========   openid\u3001\u7528\u6237\u4FE1\u606F
+`)],-1),B=e("h2",{id:"\u6CE8\u610F\u4E8B\u9879",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#\u6CE8\u610F\u4E8B\u9879","aria-hidden":"true"},"#"),n(" \u6CE8\u610F\u4E8B\u9879")],-1),P=e("p",null,"\u7F51\u9875\u6388\u6743\u56DE\u8C03\u7684\u5730\u5740\u8981\u5728\u4EE5\u4E0B\u586B\u5199\u7684\u57DF\u540D\u4E4B\u4E0B",-1),R=e("p",null,[e("img",{src:c,alt:"\u7F51\u9875\u6388\u6743\u56DE\u8C03\u7684\u5730\u5740",loading:"lazy"})],-1);function S(D,N){const t=s("ExternalLinkIcon");return u(),d("div",null,[v,q,e("p",null,[e("a",h,[_,i(t)])]),m,X,p,b,e("p",null,[e("a",f,[x,i(t)])]),g,e("p",null,[e("a",k,[w,i(t)])]),E,y,T,e("p",null,[e("a",A,[I,i(t)])]),C,z,B,P,R])}var L=o(r,[["render",S],["__file","wechat-account.html.vue"]]);export{L as default};
